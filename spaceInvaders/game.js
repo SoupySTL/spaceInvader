74777343659f3f05f6d5d2f62a36584ea11e8727
A David <alan.c.david@gmail.com>
2020-02-23 18:06:38 -0600

    add shoot sound effects

--- spaceInvaders/game.js
+++ spaceInvaders/game.js
@@ -7,13 +7,16 @@
     this.bodies = createInvaders(this).concat(new Player(this, gameSize)); 
 
     var self = this;
-    var tick = function() {
-      self.update();
-      self.draw(screen, gameSize);
-      requestAnimationFrame(tick);
-    };
+    loadSound("shoot.wav", function(shootSound) {
+      self.shootSound = shootSound;
+      var tick = function() {
+        self.update();
+        self.draw(screen, gameSize);
+        requestAnimationFrame(tick);
+      };
 
     tick();
+    });
   };
 
   Game.prototype = {
@@ -39,6 +42,14 @@
 
     addBody: function(body) {
       this.bodies.push(body);
+    },
+
+    invadersBelow: function(invader) {
+      return this.bodies.filter(function(b) {
+        return b instanceof Invader && 
+        b.center.y > invader.center.y && 
+        b.center.x - invader.center.x < invader.size.x;
+      }).length > 0;
     }
   };
 
@@ -61,6 +72,8 @@
         var bullet = new Bullet({ x: this.center.x, y: this.center.y - this.size.x - 2}, 
                                 { x: 0, y: -6});
         this.game.addBody(bullet);
+        this.game.shootSound.load();
+        this.game.shootSound.play();
       }
     }
   };
@@ -81,6 +94,13 @@
 
       this.center.x += this.speedX;
       this.patrolX += this.speedX;
+      
+      if (Math.random() > 0.995 && !this.game.invadersBelow(this)) {
+        var bullet = new Bullet({ x: this.center.x, y: this.center.y + this.size.x - 2},
+                                { x: Math.random() - 0.5, y: 2 });
+        this.game.addBody(bullet);
+        
+      }
     }
   };
 
@@ -135,6 +155,16 @@
   var colliding = function(b1, b2) {
     return !(b1 === b2 ||
              b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2);
+  };
+
+  var loadSound = function(url, callback) {
+    var loaded = function() {
+      callback(sound);
+      sound.removeEventListener('canplaythrough', loaded);
+    };
+    var sound = new Audio(url);
+    sound.addEventListener('canplaythrough', loaded);
+    sound.load();
   };
 
   window.onload = function() {
